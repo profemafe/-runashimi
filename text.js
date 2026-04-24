3a0639b6a39d48f6b43f8f15e7ec2ce103c6b541
profemafe <meriifher.x3@gmail.com>
2026-04-23 21:28:54 -0500

    Add files via upload

--- text.js
+++ text.js
@@ -1,1 +1,229 @@
-<svg xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" version="1.1" id="svg1" sodipodi:docname="exequextrerrors.svg" xml:space="preserve"><sodipodi:namedview id="namedview1" pagecolor="#ffffff" bordercolor="#000000" borderopacity="0.25" inkscape:showpageshadow="2" inkscape:pageopacity="0.0" inkscape:pagecheckerboard="0" inkscape:deskcolor="#d1d1d1" showgrid="false" fill="#000000"><inkscape:page x="0" y="0" width="20" height="20" id="page2" margin="0" bleed="0" fill="#000000"/></sodipodi:namedview><defs id="defs1" fill="#000000"/><g style="fill:#db404b;fill-opacity:1" id="g38" transform="matrix(0.025,0,0,0.025,-1.9999991,22)"><path d="m 480,-680 q 17,0 28.5,-11.5 Q 520,-703 520,-720 520,-737 508.5,-748.5 497,-760 480,-760 q -17,0 -28.5,11.5 -11.5,11.5 -11.5,28.5 0,17 11.5,28.5 11.5,11.5 28.5,11.5 z m -40,320 h 80 V -600 H 440 Z M 80,-80 v -720 q 0,-33 23.5,-56.5 Q 127,-880 160,-880 h 640 q 33,0 56.5,23.5 23.5,23.5 23.5,56.5 v 480 q 0,33 -23.5,56.5 Q 833,-240 800,-240 H 240 Z M 206,-320 H 800 V -800 H 160 v 525 z m -46,0 v -480 z" id="path1-2" style="fill:#db404b;fill-opacity:1"/></g></svg>
+/**
+ * Form iDevice
+ *
+ * Released under Attribution-ShareAlike 4.0 International License.
+ * Author: SDWEB - Innovative Digital Solutions
+ *
+ * License: http://creativecommons.org/licenses/by-sa/4.0/
+ */
+var $text = {
+    ideviceClass: 'textIdeviceContent',
+    working: false,
+    durationId: 'textInfoDurationInput',
+    durationTextId: 'textInfoDurationTextInput',
+    participantsId: 'textInfoParticipantsInput',
+    participantsTextId: 'textInfoParticipantsTextInput',
+    mainContentId: 'textTextarea',
+    feedbackTitleId: 'textFeedbackInput',
+    feedbackContentId: 'textFeedbackTextarea',
+
+    defaultBtnFeedbackText: $exe_i18n.showFeedback,
+
+    /**
+     * Engine execution order: 1
+     * Get the base html of the idevice view
+     */
+    renderView(data, accessibility, template) {
+        let content = data.textTextarea || '';
+        const feedbackContent = data[this.feedbackContentId] || '';
+
+        // Add feedback from jsonProperties only if content doesn't already have it
+        if (feedbackContent) {
+            const temp = document.createElement('div');
+            temp.innerHTML = content;
+            const hasFeedback = temp.querySelector('.feedback-button')
+                || temp.querySelector('.feedbacktooglebutton')
+                || temp.querySelector('.feedbackbutton')
+                || temp.querySelector('.feedback.js-feedback')
+                || temp.querySelector('div.feedback');
+
+            if (!hasFeedback) {
+                const btnText = c_(data[this.feedbackTitleId]) || this.defaultBtnFeedbackText;
+                content += this.createFeedbackHTML(btnText, feedbackContent);
+            }
+        }
+
+        return template.replace('{content}', content);
+    },
+
+    getHTMLView(data, pathMedia) {
+        const isInExe = eXe.app.isInExe();
+        const durationText = isInExe
+            ? c_(data[this.durationTextId])
+            : data[this.durationTextId];
+        const participantsText = isInExe
+            ? c_(data[this.participantsTextId])
+            : data[this.participantsTextId];
+
+        let infoContentHTML = '';
+        if (data[this.durationId] || data[this.participantsId]) {
+            infoContentHTML = this.createInfoHTML(
+                data[this.durationId] === '' ? '' : durationText,
+                data[this.durationId],
+                data[this.participantsId] === '' ? '' : participantsText,
+                data[this.participantsId]
+            );
+        }
+
+        let contentHtml = data[this.mainContentId];
+
+        const temp = document.createElement('div');
+        temp.innerHTML = contentHtml;
+
+        const btnDiv = temp.querySelector('.feedback-button');
+        let buttonFeedBackText = data[this.feedbackTitleId];
+        if (btnDiv) {
+            // Support both legacy eXe 2.9 (feedbackbutton) and modern (feedbacktooglebutton) formats
+            const inputEl = btnDiv.querySelector('input.feedbackbutton, input.feedbacktooglebutton');
+            if (inputEl)
+                buttonFeedBackText = isInExe
+                    ? c_(inputEl.value)
+                    : inputEl.value;
+            btnDiv.remove();
+        }
+
+        let feedBackHtml = data[this.feedbackContentId] || '';
+        const fbDiv = temp.querySelector('.feedback.js-feedback');
+        if (fbDiv) {
+            feedBackHtml = fbDiv.innerHTML;
+            fbDiv.remove();
+        }
+
+        contentHtml = temp.innerHTML;
+        if (feedBackHtml) {
+            buttonFeedBackText =
+                buttonFeedBackText === ''
+                    ? this.defaultBtnFeedbackText
+                    : buttonFeedBackText;
+            if (isInExe) buttonFeedBackText = c_(buttonFeedBackText);
+        }
+
+        data['textInfoParticipantsTextInput'] = participantsText;
+        data['textInfoDurationTextInput'] = durationText;
+        data['textTextarea'] = contentHtml;
+        data['textFeedbackInput'] = buttonFeedBackText;
+        data['textFeedbackTextarea'] = feedBackHtml;
+
+        const feedbackContentHTML =
+            feedBackHtml === ''
+                ? ''
+                : this.createFeedbackHTML(buttonFeedBackText, feedBackHtml);
+        const activityContent =
+            infoContentHTML +
+            contentHtml +
+            feedbackContentHTML;
+
+        let htmlContent = `<div class="${this.ideviceClass}">`;
+        htmlContent += this.createMainContent(activityContent);
+        htmlContent += `</div>`;
+
+        return htmlContent;
+    },
+
+    /**
+     * Engine execution order: 2
+     * Add behavior and functionalities
+     */
+    renderBehaviour(data, accessibility, ideviceId) {
+        const $node = $('#' + data.ideviceId);
+        const isInExe = eXe.app.isInExe();
+
+        const $btn = $(
+            `#${data.ideviceId} input.feedbackbutton, #${data.ideviceId} input.feedbacktooglebutton`
+        );
+        if ($btn.length === 1) {
+            const [textA, textB = textA] = $btn.val().split('|');
+            $btn.val(textA)
+                .attr('data-text-a', textA)
+                .attr('data-text-b', textB);
+            $btn.off('click')
+                .closest('.feedback-button')
+                .removeClass('clearfix');
+
+            $btn.on('click', function () {
+                if ($text.working) return false;
+                $text.working = true;
+                const btn = $(this);
+                const feedbackEl = btn
+                    .closest('.feedback-button')
+                    .next('.feedback');
+
+                if (feedbackEl.is(':visible')) {
+                    btn.val(btn.attr('data-text-a'));
+                    feedbackEl.fadeOut(() => {
+                        $text.working = false;
+                    });
+                } else {
+                    btn.val(btn.attr('data-text-b'));
+                    feedbackEl.fadeIn(() => {
+                        $text.working = false;
+                    });
+                }
+                $exeDevices.iDevice.gamification.math.updateLatex(
+                    '.exe-text-template'
+                );
+            });
+        }
+        const dataString = $node.html() || '';
+        if ($exeDevices.iDevice.gamification.math.hasLatex(dataString)) {
+            $exeDevices.iDevice.gamification.math.updateLatex(
+                '.exe-text-template'
+            );
+        }
+    },
+
+    replaceResourceDirectoryPaths(newDir, htmlString) {
+        let dir = newDir.trim();
+        if (!dir.endsWith('/')) dir += '/';
+        const custom = $('html').is('#exe-index') ? 'custom/' : '../custom/';
+
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(htmlString, 'text/html');
+        doc.querySelectorAll(
+            'img[src], video[src], audio[src], a[href]'
+        ).forEach((el) => {
+            const attr = el.hasAttribute('src') ? 'src' : 'href';
+            const val = el.getAttribute(attr).trim();
+
+            if (/^\/?files\//.test(val)) {
+                const filename = val.split('/').pop() || '';
+                if (val.indexOf('file_manager') === -1) {
+                    el.setAttribute(attr, dir + filename);
+                } else {
+                    el.setAttribute(attr, custom + filename);
+                }
+            }
+        });
+        return doc.body.innerHTML;
+    },
+
+    init(data, accessibility) {},
+
+    createMainContent(content) {
+        return `
+            <div class="exe-text-activity">
+                <div>${content}</div>
+            </div>`;
+    },
+
+    createInfoHTML(
+        durationText,
+        durationValue,
+        participantsText,
+        participantsValue
+    ) {
+        return `
+            <dl>
+                <div class="inline"><dt><span title="${durationText}">${durationText}</span></dt><dd>${durationValue}</dd></div>
+                <div class="inline"><dt><span title="${participantsText}">${participantsText}</span></dt><dd>${participantsValue}</dd></div>
+            </dl>`;
+    },
+
+    createFeedbackHTML(title, content) {
+        return `
+            <div class="iDevice_buttons feedback-button js-required">
+                <input type="button" class="feedbacktooglebutton" value="${title}">
+            </div>
+            <div class="feedback js-feedback js-hidden">${content}</div>`;
+    },
+};
